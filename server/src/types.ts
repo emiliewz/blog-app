ced2838f6c0a4c91080372d43dd667bd1f80e2aa
emilie <loveskyship@gmail.com>
2023-11-24 15:27:09 -0800

    update BaseBlog interface

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,24 +1,26 @@
 import { Types } from 'mongoose';
 
-export interface IBlog {
-  title: string,
-  author: string,
-  url: string,
-  likes: number,
-  comments: string[],
-  user: Types.ObjectId,
-}
-
 interface BaseUser {
   username: string,
   name: string,
+}
+
+interface BaseBlog {
+  title: string,
+  author: string,
+  url: string,
+}
+
+export interface IBlog extends BaseBlog {
+  likes: number,
+  comments: string[],
+  user: Types.ObjectId,
 }
 
 export interface IUser extends BaseUser {
   passwordHash: string,
   blogs?: [Types.ObjectId],
 }
-
 
 export interface UserEntry extends BaseUser {
   password: string
