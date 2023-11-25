5c4a8d1b82af73fe3fdbaf24f04c0de15ce1d5fd
emilie <loveskyship@gmail.com>
2023-11-24 16:02:51 -0800

    post blog router

--- server/src/controllers/blogs.ts
+++ server/src/controllers/blogs.ts
@@ -1,17 +1,26 @@
 import express, { RequestHandler } from 'express';
 import Blog from '../models/blog';
+import helper from '../utils/helper';
+import { IUser } from '../types';
 
 const router = express.Router();
 
 router.get('/', (async (_req, res) => {
   const blogs = await Blog
     .find({})
-    .populate('user', { username:1, name:1 });
+    .populate<{ user: IUser }>('user', { username: 1, name: 1 });
   res.json(blogs);
 }) as RequestHandler);
 
-// router.post('/', (async (req, res) => {
+router.post('/', (async (req, res) => {
+  const { title, author, url } = helper.toNewBlog(req.body);
 
-// }) as RequestHandler);
+  const blog = new Blog({
+    title, author, url,
+  });
+
+  await blog.save();
+  res.status(201).json(blog);
+}) as RequestHandler);
 
 export default router;
