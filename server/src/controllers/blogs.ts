dc80799e0d876541f18f458b613cec2b2007f23c
emilie <loveskyship@gmail.com>
2023-11-25 16:56:31 -0800

    refactor blog router, require auth to post a blog

--- server/src/controllers/blogs.ts
+++ server/src/controllers/blogs.ts
@@ -2,6 +2,8 @@ import express, { RequestHandler } from 'express';
 import Blog from '../models/blog';
 import helper from '../utils/helper';
 import { IUser } from '../types';
+import { CustomReq } from '../utils/middleware';
+import { userExtractor } from '../utils/middleware';
 
 const router = express.Router();
 
@@ -12,15 +14,28 @@ router.get('/', (async (_req, res) => {
   res.json(blogs);
 }) as RequestHandler);
 
-router.post('/', (async (req, res) => {
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.post('/', userExtractor, (async (req: CustomReq, res) => {
   const { title, author, url } = helper.toNewBlog(req.body);
+  const user = req.user;
+
+  if (!user) {
+    return res.status(401).json({ error: 'operation not permitted' });
+  }
 
   const blog = new Blog({
     title, author, url,
+    user: user._id
   });
 
   await blog.save();
-  res.status(201).json(blog);
+
+  user.blogs = user.blogs.concat(blog._id);
+  await user.save();
+
+  const createdBlog = blog.populate<{ user: IUser }>('user', { username: 1, name: 1 });
+
+  return res.status(201).json(createdBlog);
 }) as RequestHandler);
 
 export default router;
