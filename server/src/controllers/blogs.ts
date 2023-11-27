96fbb2c9dcbeca1f1b78d7f32faffc72aad6aaf3
emilie <loveskyship@gmail.com>
2023-11-26 18:21:44 -0800

    fix the problem of asyncHandler and errorHandler not working in blogs router

--- server/src/controllers/blogs.ts
+++ server/src/controllers/blogs.ts
@@ -1,118 +1,97 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import express, { RequestHandler } from 'express';
 import Blog from '../models/blog';
 import helper from '../utils/helper';
 import { IUser } from '../types';
-import { CustomReq } from '../utils/middleware';
+import { CustomReq, asyncHandler } from '../utils/middleware';
 import { userExtractor } from '../utils/middleware';
 
 const router = express.Router();
 
-router.get('/', (async (_req, res) => {
+router.use(userExtractor);
+
+router.get('/', asyncHandler(async (_req, res) => {
   const blogs = await Blog
     .find({})
     .populate<{ user: IUser }>('user', { username: 1, name: 1 });
   res.json(blogs);
 }) as RequestHandler);
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.post('/', userExtractor, (async (req: CustomReq, res) => {
-  try {
-    const { title, author, url } = helper.toNewBlog(req.body);
-    const user = req.user;
+router.post('/', asyncHandler(async (req: CustomReq, res) => {
+  const { title, author, url } = helper.toNewBlog(req.body);
+  const user = req.user;
 
-    if (!user) {
-      return res.status(401).json({ error: 'operation not permitted' });
-    }
-
-    const blog = new Blog({
-      title, author, url,
-      user: user._id
-    });
-
-    await blog.save();
-
-    user.blogs = user.blogs.concat(blog._id);
-    await user.save();
-
-    const createdBlog = blog.populate<{ user: IUser }>('user', { username: 1, name: 1 });
-
-    return res.status(201).json(createdBlog);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    return res.status(400).send(errorMessage);
+  if (!user) {
+    return res.status(401).json({ error: 'operation not permitted' });
   }
+
+  const blog = new Blog({
+    title, author, url,
+    user: user._id
+  });
+
+  await blog.save();
+  user.blogs = user.blogs.concat(blog._id);
+  await user.save();
+
+  const createdBlog = await Blog.findById(blog._id).populate<{ user: IUser }>('user', { username: 1, name: 1 });
+  return res.status(201).json(createdBlog);
 }) as RequestHandler);
 
-router.put('/:id', (async (req, res) => {
-  try {
-    const { title, author, url } = helper.toNewBlog(req.body);
+router.put('/:id', asyncHandler(async (req: CustomReq, res) => {
+  const { title, author, url } = helper.toNewBlog(req.body);
+  const user = req.user;
+  const blog = await Blog.findById(req.params.id);
 
-    await Blog.findByIdAndUpdate(req.params.id, {
+  if (!blog) {
+    return res.status(400).json({ error: 'blog not find' });
+  } else if (!user || blog.user.toString() !== user._id.toString()) {
+    return res.status(401).json({ error: 'operation not permitted' });
+  }
+
+  const updatedBlog = await Blog
+    .findByIdAndUpdate(req.params.id, {
       title, author, url
-    }, { new: true });
+    }, { new: true, runValidators: true, context: 'query' })
+    .populate<{ user: IUser }>('user', { username: 1, name: 1 });
 
-    const updatedBlog = await Blog.findById(req.params.id).populate<{ user: IUser }>('user', { username: 1, name: 1 });
-
-    return res.json(updatedBlog);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    return res.status(400).send(errorMessage);
-  }
+  return res.json(updatedBlog);
 }) as RequestHandler);
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.delete('/:id', userExtractor, (async (req: CustomReq, res) => {
-  try {
-    const user = req.user;
-    const blog = await Blog.findById(req.params.id);
+router.delete('/:id', asyncHandler(async (req: CustomReq, res) => {
+  const user = req.user;
+  const blog = await Blog.findById(req.params.id);
 
-    if (!user || blog?.user.toString() !== user._id.toString()) {
-      return res.status(401).json({ error: 'operation not permitted' });
-    } else if (!blog) {
-      return res.status(400).json({ error: 'blog not find' });
-    }
-
-    user.blogs = user.blogs.filter(b => b.toString() !== blog._id.toString());
-
-    await user.save();
-
-    await blog.deleteOne();
-
-    return res.status(204).end();
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    return res.status(400).send(errorMessage);
+  if (!blog) {
+    return res.status(400).json({ error: 'blog not find' });
+  } else if (!user || blog.user.toString() !== user._id.toString()) {
+    return res.status(401).json({ error: 'operation not permitted' });
   }
+
+  user.blogs = user.blogs.filter(b => b.toString() !== blog._id.toString());
+  await user.save();
+
+  await blog.deleteOne();
+
+  return res.status(204).end();
 }) as RequestHandler);
 
-router.post('/:id/comments', (async (req, res) => {
-  try {
-    const { comment } = helper.toNewComent(req.body);
+router.post('/:id/comments', asyncHandler(async (req: CustomReq, res) => {
+  const user = req.user;
+  const { comment } = helper.toNewComent(req.body);
 
-    const blog = await Blog.findById(req.params.id);
+  const blog = await Blog.findById(req.params.id);
 
-    if (!blog) return res.status(400).json({ error: 'blog not found' });
-    blog.comments = blog.comments.concat(comment);
-
-    await blog.save();
-
-    return res.json(blog);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    return res.status(400).send(errorMessage);
+  if (!blog) {
+    return res.status(400).json({ error: 'blog not found' });
+  } else if (!user) {
+    return res.status(401).json({ error: 'operation not permitted' });
   }
+
+  blog.comments = blog.comments.concat(comment);
+  await blog.save();
+
+  return res.json(blog);
 }) as RequestHandler);
 
 export default router;
