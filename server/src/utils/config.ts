911fca6a4b1f9a881b0b300eb9a109ddd6403031
emilie <loveskyship@gmail.com>
2023-11-28 23:26:01 -0800

    define a test MongodeURI for test environment

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,16 +1,20 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-declare let process : {
+declare let process: {
   env: {
     PORT: string,
     MONGODB_URI: string,
     SECRET: string,
+    TEST_MONGODB_URI: string,
+    NODE_ENV: string,
   }
 };
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
 
 export default {
