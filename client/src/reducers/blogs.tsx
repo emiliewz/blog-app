b2349892fd332eea2349fef70d699b32703a37c5
emilie <loveskyship@gmail.com>
2023-12-10 13:54:55 -0800

    implement blogSlice action - delete blogs

--- client/src/reducers/blogs.tsx
+++ client/src/reducers/blogs.tsx
@@ -10,42 +10,42 @@ export type blogSliceState = {
 
 export const initialState = [
   {
-    id: 1,
+    id: 0,
     title: 'Canonical string reduction',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
     likes: 12,
   },
   {
-    id: 2,
+    id: 1,
     title: 'First class tests',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
     likes: 10,
   },
   {
-    id: 3,
+    id: 2,
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
   },
   {
-    id: 4,
+    id: 3,
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
   },
   {
-    id: 5,
+    id: 4,
     title: 'TDD harms architecture',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
     likes: 0,
   },
   {
-    id: 6,
+    id: 5,
     title: 'Type wars',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
@@ -65,9 +65,13 @@ const blogSlice = createSlice({
     },
     update(state, { payload }: PayloadAction<blogSliceState>) {
       return state.map(b => b.id !== payload.id ? b : payload);
+    },
+    remove(state, { payload }: PayloadAction<number>) {
+      const id = payload;
+      return state.filter(b => b.id !== id);
     }
   }
 });
 
-export const { set, create, update } = blogSlice.actions;
+export const { set, create, update, remove } = blogSlice.actions;
 export default blogSlice.reducer;
