1463a50d49f3ec81c044b4fd7ee6c29f126523d5
emilie <loveskyship@gmail.com>
2023-12-10 13:44:04 -0800

    test blogSlice update action

--- client/src/reducers/blogReducer.test.ts
+++ client/src/reducers/blogReducer.test.ts
@@ -30,5 +30,28 @@ describe('blogReducer', () => {
     expect(newState).toContainEqual(action.payload);
   });
 
+  test('returns updated state with action blogs/update', () => {
+    const state: blogSliceState[] = initialState.slice(0, 4);
+
+    const blogToUpdate = {
+      id: 1,
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 20,
+    };
+
+    const action = {
+      type: 'blogs/update',
+      payload: blogToUpdate
+    };
+
+    deepFreeze(state);
+    const newState = blogReducer(state, action);
+
+    expect(newState).toHaveLength(4);
+    expect(newState).toContainEqual(initialState[1]);
+    expect(newState).toContainEqual(blogToUpdate);
+  });
 
 });
