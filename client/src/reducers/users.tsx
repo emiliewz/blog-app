5e1af1f01f96f3e40bba0742125f2d82e64a587f
emilie <loveskyship@gmail.com>
2023-12-11 09:08:26 -0800

    usersSlice action - set users

---
+++ client/src/reducers/users.tsx
@@ -0,0 +1,46 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { blogSliceState } from './blogs';
+
+export interface usersSliceState {
+  username: string,
+  password?: string,
+  name: string
+  id: string
+  blogs: blogSliceState[]
+}
+
+export const initialState: usersSliceState[] = [
+  {
+    username: 'Alice',
+    name: 'alice',
+    id: '0',
+    blogs: []
+  },
+  {
+    username: 'Bob',
+    name: 'bob',
+    id: '1',
+    blogs: []
+  },
+  {
+    username: 'Charles',
+    name: 'charles',
+    id: '2',
+    blogs: []
+  }
+];
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    set(_state, { payload }: PayloadAction<usersSliceState[]>) {
+      return payload;
+    }
+  }
+});
+
+export const { set } = usersSlice.actions;
+
+export default usersSlice.reducer;
+
