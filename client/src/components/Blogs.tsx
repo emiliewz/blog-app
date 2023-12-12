de88fe494446955ba66af2f7d5c6446b54b94876
emilie <loveskyship@gmail.com>
2023-12-12 15:24:13 -0800

    sort blogs by likes

--- client/src/components/Blogs.tsx
+++ client/src/components/Blogs.tsx
@@ -1,18 +1,34 @@
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../app/hooks';
+import { Table } from 'react-bootstrap';
+import { BlogsSliceState } from '../app/types';
 
 const Blogs = () => {
   const blogs = useAppSelector(({ blogs }) => blogs);
 
+  const byLikes = (a: BlogsSliceState, b: BlogsSliceState): number => b.likes - a.likes;
+
   return (
     <div>
-      {blogs.map(b => (
-        <p key={b.id}>
-          <Link to={`blogs/${b.id}`}>
-            {b.title}
-          </Link>
-        </p>
-      ))}
+      <h2>Blogs</h2>
+
+      <Table striped>
+        <tbody>
+          {[...blogs].sort(byLikes).map(b => (
+            <tr key={b.id}>
+              <td>
+                <Link className='text-decoration-none' to={`/blogs/${b.id}`}>
+                  {b.title}
+                </Link>
+              </td>
+              <td>
+                {b.author}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+
     </div>
   );
 };
