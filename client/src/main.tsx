51e4a1e954bc483ed5e2e87e13b82a4f489d8a16
emilie <loveskyship@gmail.com>
2023-12-11 16:28:35 -0800

    provide the redux store to React

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom/client';
+
 import App from './app/App.tsx';
+import store from './app/store.tsx';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
