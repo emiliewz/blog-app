86555a99dcd9c8a6868ab667f1db5d91898e006b
emilie <loveskyship@gmail.com>
2023-12-12 16:48:10 -0800

    useField hook

--- client/src/app/hooks.tsx
+++ client/src/app/hooks.tsx
@@ -1,8 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
+import { useState } from 'react';
 
 type DispatchFunc = () => AppDispatch;
 export const useAppDispatch: DispatchFunc = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
+export const useField = (type: string) => {
+  const [value, setValue] = useState<string>('');
+
+  const onChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange
+  };
+};
