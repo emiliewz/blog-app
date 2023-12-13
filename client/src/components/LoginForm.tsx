501af348dcdbb43055d46750f045eaac3c2886b5
emilie <loveskyship@gmail.com>
2023-12-13 09:51:55 -0800

    implement the handle submit function to login a user

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,15 +1,34 @@
-import { useField } from '../app/hooks';
+import { FormEvent, FormEventHandler } from 'react';
+import { useAppDispatch, useField } from '../app/hooks';
+import storageService from '../services/storage';
+import loginService from '../services/login';
 import { Button, Form } from 'react-bootstrap';
+import { set } from '../reducers/user';
 
 const LoginForm = () => {
   const username = useField('text');
   const password = useField('password');
+  const dispatch = useAppDispatch();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username: username.value,
+        password: password.value
+      });
+      storageService.saveUser(user);
+      dispatch(set(user));
+    } catch (exception) {
+      console.log('wrong username or password');
+    }
+  };
 
   return (
     <div>
       <h2>log in to application</h2>
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label>username:</Form.Label>
           <Form.Control
