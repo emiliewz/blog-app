5d1d8fdb2e821496c6d2d9c2a4bce4e0061b0624
emilie <loveskyship@gmail.com>
2023-12-12 17:32:31 -0800

    single user view

---
+++ client/src/components/User.tsx
@@ -0,0 +1,32 @@
+import { useAppSelector } from '../app/hooks';
+import { Link, useParams } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+
+const User = () => {
+  const { id } = useParams<{ id?: string }>();
+  const user = useAppSelector(({ users }) => users.find(u => u.id === id));
+
+  if (!user) return null;
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <h3>added blogs</h3>
+      <Table striped>
+        <tbody>
+          {user.blogs.map((b, i) => (
+            <tr key={b.id}>
+              <td>
+                <Link className='text-decoration-none' to={`/blogs/${b.id}`}>
+                  {i + 1}. {b.title}
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div >
+  );
+};
+
+export default User;
