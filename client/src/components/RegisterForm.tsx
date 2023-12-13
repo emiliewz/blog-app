d69b0d2dcd85c22001731ff25d5fcd9102e0718d
emilie <loveskyship@gmail.com>
2023-12-13 10:41:27 -0800

    implement the handle submit function to register the app

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,16 +1,36 @@
-import { useField } from '../app/hooks';
+import { FormEvent, FormEventHandler } from 'react';
+import { useAppDispatch, useField } from '../app/hooks';
 import { Button, Form } from 'react-bootstrap';
+import { set } from '../reducers/user';
+import storageService from '../services/storage';
+import usersService from '../services/users';
 
 const RegisterForm = () => {
   const name = useField('text');
   const username = useField('text');
   const password = useField('password');
+  const dispatch = useAppDispatch();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const user = await usersService.create({
+        name: name.value,
+        username: username.value,
+        password: password.value
+      });
+      storageService.saveUser(user);
+      dispatch(set(user));
+    } catch (exception) {
+      console.log('something wrong happened');
+    }
+  };
 
   return (
     <div>
       <h2>Register the application</h2>
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label>name:</Form.Label>
           <Form.Control
