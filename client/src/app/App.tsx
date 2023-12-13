ec11223a4e7fad8d742c5c82195fa9990495884c
emilie <loveskyship@gmail.com>
2023-12-12 17:34:26 -0800

    initialize users, define individual user routes and all users routes

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,8 +3,12 @@ import Blogs from '../components/Blogs';
 import blogsService from '../services/blogs';
 import { useAppDispatch } from './hooks';
 import { set } from '../reducers/blogs';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import Blog from '../components/Blog';
+import User from '../components/User';
+import Users from '../components/Users';
+import usersService from '../services/users';
+import { set as setUsers } from '../reducers/users';
 
 const App = () => {
   const dispatch = useAppDispatch();
@@ -14,15 +18,20 @@ const App = () => {
       .then(data => {
         dispatch(set(data));
       });
+    usersService.getAll()
+      .then(data => dispatch(setUsers(data)));
   }, []);
 
   return (
     <div>
       <h1>hello</h1>
-
+      <Link to='/'>blogs</Link>
+      <Link to='/users'>Users</Link>
       <Routes>
         <Route path='/' element={<Blogs />} />
         <Route path='/blogs/:id' element={<Blog />} />
+        <Route path='/users/:id' element={<User />} />
+        <Route path='/users' element={<Users />} />
       </Routes>
     </div>
   );
