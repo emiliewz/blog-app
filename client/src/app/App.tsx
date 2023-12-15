0f385779226d112f9a6934cb283a57c0c8c64062
emilie <loveskyship@gmail.com>
2023-12-14 22:03:16 -0800

    implement responsive navigation header

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
-import { useAppDispatch } from './hooks';
+import { useAppDispatch, useAppSelector } from './hooks';
 import { initializeBlogs } from '../reducers/blogs';
 import { Link, Route, Routes } from 'react-router-dom';
 import { initializeUsers } from '../reducers/users';
-import { initializeUser } from '../reducers/user';
+import { initializeUser, logOut } from '../reducers/user';
 
 import LoginForm from '../components/LoginForm';
 import Blogs from '../components/Blogs';
@@ -14,9 +14,11 @@ import Users from '../components/Users';
 import Notification from '../components/Notification';
 import RegisterForm from '../components/RegisterForm';
 import NewBlog from '../components/NewBlog';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 
 const App = () => {
   const dispatch = useAppDispatch();
+  const user = useAppSelector(({ user }) => user);
 
   useEffect(() => {
     dispatch(initializeBlogs());
@@ -25,14 +27,31 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <h1> Blog App </h1>
-      <Link to='/'>blogs</Link>
-      <Link to='/users'>Users</Link>
-      <Link to='/create'>Create</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/register'>Register</Link>
+    <div className='container'>
+      <Navbar bg='dark' expand='lg' data-bs-theme='dark' className='bg-body-tertiary'>
+        <Container>
+          <Navbar.Brand as={Link} to='/'>Blog App</Navbar.Brand>
+          <Navbar.Collapse>
+            <Nav className='me-auto'>
+              <Nav.Link as={Link} to='/'>Blogs</Nav.Link>
+              <Nav.Link as={Link} to='/create'>Create</Nav.Link>
+              <Nav.Link as={Link} to='/users'>Users</Nav.Link>
+              {!user && (<><Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                <Nav.Link as={Link} to='/register'>Register</Nav.Link></>)}
+            </Nav>
+          </Navbar.Collapse>
 
+          {user && (<>
+            <Navbar.Collapse className='justify-content-end'>
+              <Navbar.Text>
+                Signed in as: {user?.name}
+              </Navbar.Text>
+              <Button className='ms-2' variant='outline-success' onClick={() => dispatch(logOut())}>LogOut</Button>
+            </Navbar.Collapse>
+            <Navbar.Toggle />
+          </>)}
+        </Container>
+      </Navbar>
       <Notification />
 
       <Routes>
@@ -44,7 +63,7 @@ const App = () => {
         <Route path='/login' element={<LoginForm />} />
         <Route path='/register' element={<RegisterForm />} />
       </Routes>
-    </div>
+    </div >
   );
 };
 
