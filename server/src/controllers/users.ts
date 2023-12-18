094f7644cecd997f41e08352f98e7a201a47d730
emilie <loveskyship@gmail.com>
2023-12-18 12:26:41 -0800

    allow users router to use userExtractor, and define a /check endpoint to check if token is valid

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import bcrypt from 'bcrypt';
 import express, { RequestHandler } from 'express';
 import User from '../models/user';
 import { IBlog } from '../types';
 import helper from '../utils/helper';
-import { asyncHandler } from '../utils/middleware';
+import { CustomReq, asyncHandler } from '../utils/middleware';
+import { userExtractor } from '../utils/middleware';
 
 const router = express.Router();
+router.use(userExtractor);
 
 router.get('/', (async (_req, res) => {
   const users = await User.find({})
@@ -13,7 +16,6 @@ router.get('/', (async (_req, res) => {
   res.json(users);
 }) as RequestHandler);
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 router.post('/', asyncHandler(async (req, res) => {
   const { username, name, password } = helper.toNewUser(req.body);
 
@@ -31,5 +33,11 @@ router.post('/', asyncHandler(async (req, res) => {
   const savedUser = await user.save();
   return res.status(201).json(savedUser);
 }) as RequestHandler);
+
+router.get('/check', asyncHandler((req: CustomReq, res) => {
+  const user = req.user;
+  if (!user) return res.status(400).json({ error: 'token expired' });
+  return res.status(200).json({ message: 'valid token' });
+}));
 
 export default router;
