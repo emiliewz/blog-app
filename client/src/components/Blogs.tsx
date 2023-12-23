58863be10921d3419bc1ac205bdbc83ab72469ed
emilie <loveskyship@gmail.com>
2023-12-23 15:13:28 -0800

    update blogs view to allow sorting by latest blogs

--- client/src/components/Blogs.tsx
+++ client/src/components/Blogs.tsx
@@ -1,21 +1,23 @@
 import { Link } from 'react-router-dom';
-import { byComments, byLikes, useAppSelector } from '../app/hooks';
+import { assertNever, byComments, byCreateDate, byLikes, useAppSelector } from '../app/hooks';
 import { Dropdown, DropdownButton, Table } from 'react-bootstrap';
-import { BlogsSliceState } from '../app/types';
+import { BlogsSliceState, SortType } from '../app/types';
 import { useState } from 'react';
 
 const Blogs = () => {
   const blogs: BlogsSliceState[] = useAppSelector(({ blogs }) => blogs);
-  const [sort, setSort] = useState<string>('Likes');
+  const [sort, setSort] = useState<SortType>(SortType.Likes);
 
-  const blogsSorted = () => {
+  const getSortedBlogs = () => {
     switch (sort) {
-      case 'Likes':
+      case SortType.Likes:
         return [...blogs].sort(byLikes);
-      case 'Comments':
+      case SortType.Comments:
         return [...blogs].sort(byComments);
+      case SortType.Recently:
+        return [...blogs].sort(byCreateDate);
       default:
-        return [...blogs].sort(byComments);
+        return assertNever(sort);
     }
   };
 
@@ -23,12 +25,13 @@ const Blogs = () => {
     <div>
       <h2>Blogs</h2>
       <DropdownButton variant='outline-info' size='sm' title={`Sort by: Most ${sort}`}>
-        <Dropdown.Item eventKey="1" onClick={() => setSort('Likes')}>Most Likes</Dropdown.Item>
-        <Dropdown.Item eventKey="2" onClick={() => setSort('Comments')}>Most Comments</Dropdown.Item>
+        <Dropdown.Item eventKey="1" onClick={() => setSort(SortType.Likes)}>Most Likes</Dropdown.Item>
+        <Dropdown.Item eventKey="2" onClick={() => setSort(SortType.Comments)}>Most Comments</Dropdown.Item>
+        <Dropdown.Item eventKey="2" onClick={() => setSort(SortType.Recently)}>Most Recently</Dropdown.Item>
       </DropdownButton>
       <Table striped>
         <tbody>
-          {blogsSorted().map(b => (
+          {getSortedBlogs().map(b => (
             <tr key={b.id}>
               <td>
                 <Link className='text-decoration-none' to={`/blogs/${b.id}`}>
